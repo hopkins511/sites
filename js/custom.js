05bda36d0deb6b8de6cbdc214f6c5959f22b2d8d
Bob Hopkins <34924003+hopkins511@users.noreply.github.com>
2021-09-07 15:56:59 -0500

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -67,3 +67,14 @@ $( document ).ready(function() {
 	});
 
 });
+
+$(window).load(function() {
+	$(".loader").delay(2000).fadeOut("slow");
+	  $("#overlayer").delay(2000).fadeOut("slow");
+})
+
+setTimeout(
+	function() 
+	{
+		$("body").css("overflow", "inherit");
+	}, 2000);
